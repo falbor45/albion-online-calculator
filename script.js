fc669750eca74b391425f6713de3701c2d9037e2
Artur Falborski <falborski.artur@gmail.com>
2017-04-24 17:18:18 +0200

    added first calculator (fame per hour)

---
+++ script.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    console.log('Ready to go!');
+    var $fpht = $('#fpht');
+    var $fph = $('#fph');
+    $('#calculateFpH').click(function() {
+        if ($fph.val() !== '' && $fpht.val() !== '') {
+            $('.fph-result').text(($fph.val() / $fpht.val()) * 60);
+            $('.fph-fox-calculator').text('That equals to killing ' + (Math.ceil((($fph.val() / $fpht.val()) * 60) / 18)) + ' foxes per hour!');
+            $('.fph-pop-up').removeClass('hidden');
+            $('.dimmer').removeClass('hidden');
+            $fph.val('');
+            $fpht.val('');
+        } else {
+            $fph.val('');
+            $fpht.val('');
+            $('#calculateFpH').popover('show');
+            setTimeout(function() {
+                $('#calculateFpH').popover('hide');
+            }, 1000);
+            return false;
+        }
+    });
+    $('#close-fph-pop-up').click(function() {
+        $('.fph-pop-up').addClass('hidden');
+        $('.dimmer').addClass('hidden');
+    })
+});
